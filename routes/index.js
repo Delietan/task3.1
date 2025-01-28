c023ce2c873bec4dcf6dc63699497881dbeafd98
Delietan <martinusnord@gmail.com>
2025-01-29 00:19:56 +0100

    more

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(`
+    <h2>Choose an option:</h2>
+    <ul>
+    <li><a href="http://localhost:3000/message">Welcome</a></li>
+    <li><a href="http://localhost:3000/about">About</a></li>
+    <li><a href="http://localhost:3000/random-number">Random Number</a></li>
+    </ul>
+  `);
+});
+
+router.get('/message', function(req, res, next) {
+  res.send('Welcome');
+});
+
+router.get('/about', function(req, res, next) {
+  res.send( 'My name is Martinus and this is a test project to play around with the functionality of Render.com' );
+});
+
+router.get('/random-number', function(req, res, next) {
+  let randomNumber = Math.round(Math.random() * 10)
+  res.send('Here is the random number you requested sir: ' + randomNumber );
 });
 
 module.exports = router;
